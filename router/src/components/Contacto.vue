8c08240676222bfd6e6b0d7cb20180c3b5d0b53c
Trianlos91 <flucec@gmail.com>
2019-01-01 20:33:02 -0300

    programmatic nav

--- router/src/components/Contacto.vue
+++ router/src/components/Contacto.vue
@@ -1,3 +1,20 @@
 <template>
-  <h2>Contacto</h2>
+  <section>
+    <h2>Contacto</h2>
+    <button @click="volverHome()">Volver a Home</button>
+  </section>
+
 </template>
+
+<script>
+  export default {
+    created(){
+      setTimeout(()=> this.$router.push('/'),2000);
+    },
+    methods: {
+      volverHome(){
+        this.$router.push('/'); //home
+      }
+    }
+  }
+</script>
