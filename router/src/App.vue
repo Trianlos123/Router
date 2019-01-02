d4f4ff0700cdd4554c3b7f9ba04ea1bcb2ac6559
Trianlos91 <flucec@gmail.com>
2019-01-01 22:31:25 -0300

    dynamic router

--- router/src/App.vue
+++ router/src/App.vue
@@ -3,6 +3,7 @@
     <h1>Rutas con Vue Router</h1>
     <router-link to="/" exact>Home</router-link>
     <router-link to="/contacto">Contacto</router-link>
+    <router-link to="/usuario/77">Usuario</router-link>
     
     <hr>
     <router-view></router-view>
--- router/src/routes.js
+++ router/src/routes.js
@@ -1,8 +1,10 @@
 import Home from './components/Home.vue';
 import Contacto from './components/Contacto.vue';
+import Usuario from './components/Usuario.vue';
 
 export const routes = [
   {path: '/', component: Home},
   {path: '/contacto', component: Contacto},
+  {path: '/usuario/:id', component: Usuario},
 ]
 
